e3e43b3b9554fd1b2e4b53ae35b460e35ed296bc
Niall Siegenheim <sniall@student.ethz.ch>
2022-07-14 09:34:53 +0200

    present

--- openacc/practicals/axpy/axpy_openacc.f90
+++ openacc/practicals/axpy/axpy_openacc.f90
@@ -22,7 +22,7 @@ subroutine axpy_gpu(n, alpha, x, y)
   integer i
 
   ! TODO: Offload this loop to the GPU
-  !$acc parallel loop
+  !$acc parallel loop present(x,y)
   do i = 1,n
      y(i) = y(i) + alpha*x(i)
   enddo
