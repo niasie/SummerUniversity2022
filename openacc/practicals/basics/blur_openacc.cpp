5374a3bd2108fb3219810643ee45a924760357ab
Niall Siegenheim <sniall@student.ethz.ch>
2022-07-14 10:04:14 +0200

    fix

--- openacc/practicals/basics/blur_openacc.cpp
+++ openacc/practicals/basics/blur_openacc.cpp
@@ -75,20 +75,20 @@ void blur_twice_gpu_nocopies(double *in, double *out, int n, int nsteps) {
       int i;
 
 // TODO: Offload the following loop to the GPU
-#pragma acc parallel loop
+#pragma acc parallel loop present(in, out)
       for (i = 1; i < n - 1; ++i) {
         buffer[i] = blur(i, in);
       }
 
 // TODO: Offload the following loop to the GPU
-#pragma acc parallel loop
+#pragma acc parallel loop present(in, out)
       for (i = 2; i < n - 2; ++i) {
         out[i] = blur(i, buffer);
       }
 
 // TODO: Offload the following loop to the GPU
 //       Is it possible to just swap the pointers here?
-#pragma acc parallel loop
+#pragma acc parallel loop present(in, out)
       for (i = 0; i < n; ++i) {
         in[i] = out[i];
       }
